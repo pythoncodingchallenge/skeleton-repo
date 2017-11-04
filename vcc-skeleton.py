229490e77532bcfd81951cd03db1064f02cdb02c
Kunal Wagle <kmw13@ic.ac.uk>
2017-11-04 19:12:03 +0000

    Initial

---
+++ vcc-skeleton.py
@@ -0,0 +1,35 @@
+# ONLY EDIT FUNCTIONS MARKED CLEARLY FOR QUESTIONS 1 AND 2. DO NOT CHANGE ANY METHOD SIGNATURES OR THE RUNALL METHOD
+
+from flask import Flask, request, jsonify
+import json
+from time import clock
+
+app = Flask(__name__)
+
+
+# IMPORTANT: DO NOT CHANGE THIS FUNCTION
+@app.route('/runall', methods=['POST'])
+def runall():
+    q1inputs = request.data
+    # q2inputs = request.args.get('q2inputs')
+    q1inputs = json.loads(q1inputs)
+    response = jsonify([[runq1(q1input) for q1input in q1inputs], []])
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+def runq1(q1input):
+    start = clock()
+    output = findbestprofit(q1input[0], q1input[1])
+    end = clock()
+    diff = end - start
+    return output, diff
+
+
+# Space for question 1
+def findbestprofit(pricesarray, k):
+    return -1
+
+
+if __name__ == '__main__':
+    app.run(port=8182)
